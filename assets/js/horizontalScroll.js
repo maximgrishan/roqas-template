11c4e154f11bf2342623f2e106464aade82179d2
Maxim Grishan <maksimka.grishan@mail.ru>
2020-07-18 13:58:23 +0300

    version 0.1

---
+++ assets/js/horizontalScroll.js
@@ -0,0 +1,17 @@
+(function() {
+	// function scrollHorizontally(e) {
+		// e = window.event || e;
+		// var delta = Math.max(-1, Math.min(1, (e.wheelDelta || -e.detail)));
+		// document.getElementById('horizontal-scroll-wrapper').scrollLeft -= (delta*40); // Multiplied by 40
+		// e.preventDefault();
+	// }
+	// if (document.getElementById('horizontal-scroll-wrapper').addEventListener) {
+		// IE9, Chrome, Safari, Opera
+		// document.getElementById('horizontal-scroll-wrapper').addEventListener("mousewheel", scrollHorizontally, false);
+		Firefox
+		// document.getElementById('horizontal-scroll-wrapper').addEventListener("DOMMouseScroll", scrollHorizontally, false);
+	// } else {
+		// IE 6/7/8
+		// document.getElementById('horizontal-scroll-wrapper').attachEvent("onmousewheel", scrollHorizontally);
+	// }
+})();
